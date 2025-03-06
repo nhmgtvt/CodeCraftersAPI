1b36532df29c7f2fa3a2cc19f4d3a6a286a43d29
n10965092 <nhmgtvt@gmail.com>
2025-03-07 01:10:59 +1000

    Remove unused ref

--- CodeCrafters.Application/Entities/Auth/LoginRequest.cs
+++ CodeCrafters.Application/Entities/Auth/LoginRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeCrafters.Application.Entities.Auth
+﻿namespace CodeCrafters.Application.Entities.Auth
 {
     public class LoginRequest
     {
--- CodeCrafters.Infrastructure/Repositories/UserRepository.cs
+++ CodeCrafters.Infrastructure/Repositories/UserRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CodeCrafters.Domain.Entities.Auth;
+﻿using CodeCrafters.Domain.Entities.Auth;
 using CodeCrafters.Domain.Interfaces.Auth;
 using CodeCrafters.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
