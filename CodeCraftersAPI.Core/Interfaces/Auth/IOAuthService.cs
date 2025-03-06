7fe09a38fe8ea25c54173201e82ca5a2f7c72599
n10965092 <nhmgtvt@gmail.com>
2025-03-07 01:08:50 +1000

    Remove unused ref

--- CodeCraftersAPI.Core/Interfaces/Auth/IOAuthService.cs
+++ CodeCraftersAPI.Core/Interfaces/Auth/IOAuthService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeCrafters.Domain.Interfaces.Auth
+﻿namespace CodeCrafters.Domain.Interfaces.Auth
 {
     public interface IOAuthService
     {
